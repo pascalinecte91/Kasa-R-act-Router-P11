e850f58f1727759f3ab807c23d534919f3265907
pascaline <pascalinecte@gmail.com>
2022-11-06 23:35:41 +0100

    🚑 path-pages-nav-assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
     return (
-        <div>
-            <h1>hello</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} /> 
+                <Route path="/404" element={<NotFoundPage />} />
+                {/* le chemin avec * url qui ne correspond à rien de declaré au dessus */}
+                <Route path="*" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
---
+++ src/pages/Sheet.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Sheet = () => {
+    return (
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+                facere!
+            </p>
+        </div>
+    );
+};
+
+export default Sheet;
